d61a43d44d9582209a5bea0593c5f01e53f111ca
Jonathan M. Lane <jonathan@thinkingbig.net>
2016-03-19 16:32:52 -0300

    Adding Steam login component with official button

---
+++ src/components/login-steam.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SteamLogin extends React.Component {
+  render() {
+    return (
+      <a>
+        <img src="http://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_small.png" border="0"/>
+      </a>
+    );
+  }
+}
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SteamLogin from './login-steam.js';
 
 export default class NavBar extends React.Component {
   render() {
@@ -7,16 +8,16 @@ export default class NavBar extends React.Component {
         <div className="navbar-header">
           <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
+            <span className="icon-bar"/>
+            <span className="icon-bar"/>
+            <span className="icon-bar"/>
           </button>
           <a className="navbar-brand" href="#">Athena Security Group Attendance</a>
         </div>
 
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul className="nav navbar-nav navbar-right">
-            <li></li>
+            <li><SteamLogin/></li>
           </ul>
         </div>
       </nav>
