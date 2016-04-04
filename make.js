4fe9316738286cf92f50c421d9a33e684d17bcca
Jonathan M. Lane <jmlane@gmail.com>
2016-04-04 20:48:50 +0000

    Cleaning-up statics and make target

--- make.js
+++ make.js
@@ -1,12 +1,17 @@
+/* global target cd mkdir cp rm */
 require('shelljs/make');
-/* global target cd mkdir cp */
+const config = require('./package.json').config;
 
-const OUTPUT_DIR = process.env.npm_package_config_output;
-const STATIC_DIR = process.env.npm_package_config_static;
-
+const OUTPUT_DIR = config.output;
+const STATIC_DIR = config.static;
 
 target.static = () => {
-    cd(__dirname);
-    mkdir('-p', OUTPUT_DIR);
-    cp('-R', STATIC_DIR, OUTPUT_DIR);
-}
+  cd(__dirname);
+  mkdir('-p', OUTPUT_DIR);
+  cp('-R', STATIC_DIR, OUTPUT_DIR);
+};
+
+target.clean = () => {
+  cd(__dirname);
+  rm('-R', OUTPUT_DIR);
+};
