183adffd128dbf9620e0f968f7212b7a138b696d
Jonathan M. Lane <jonathan@thinkingbig.net>
2016-04-19 00:54:29 -0300

    WIP: added react-router and started using example code boilerplate

--- routes/react.js
+++ routes/react.js
@@ -3,17 +3,30 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from '../components/app.js';
 import Page from '../components/page.js';
+import { match } from 'react-router';
 
 const router = Router();
 
 router.all('*', (req, res) => {
-  res.send(ReactDOMServer.renderToStaticMarkup(
-    <Page>
-      <div id="app" dangerouslySetInnerHTML={
-        {__html: ReactDOMServer.renderToString(<App/>)}
-      }/>
-    </Page>
-  ));
+  match({routes, location:req.url}, (err, redirect, props) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else if (redirect) {
+      res.redirect(redirect.pathname + redirect.search);
+    } else if (props) {
+      res.send(ReactDOMServer.renderToStaticMarkup(
+        <Page>
+          <div id="app" dangerouslySetInnerHTML={
+            {__html: ReactDOMServer.renderToString(<App {...props}/>)}
+          }/>
+        </Page>
+      ));
+    } else {
+      res.status(404).send('Not Found');
+    }
+
+
+  });
 });
 
 export default router;
