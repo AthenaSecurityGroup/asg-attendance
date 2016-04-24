720fea4a81397b409a69cd32bae2d447b7250521
Jonathan M. Lane <jonathan@thinkingbig.net>
2016-04-24 17:52:46 -0300

    React routing for client

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
+import routes from './routes/router.js';
 
 render(
   <Router routes={routes} history={browserHistory}/>,
